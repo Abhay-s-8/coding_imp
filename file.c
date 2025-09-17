febebec2c84112e0e6f76c12ea5fec23167fe6f9
Abhay Singh <abhay2662006@gmail.com>
2025-09-17 14:41:23 +0530

    uf
    ijgfuih

---
+++ file.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    struct node{
+        int data;
+        struct node *next;
+
+    };
+
+    struct node *head=NULL,*temp, *newnode;
+
+    int choice = 1;
+    while (choice){
+        newnode = (struct node*)malloc(sizeof(struct node));
+
+        printf(
+            "enter data :"
+        );
+
+        scanf("%d",&newnode->data);
+        if(head == 0){
+            head = temp = newnode;
+        }
+        else{
+            temp->next= newnode;
+            temp = newnode;
+        }
+
+        temp  -> next = head;
+        printf("Do u Want to continue press 1 otherwise 0: ");
+        scanf("%d",&choice);
+    }
+}
