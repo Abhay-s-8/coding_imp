fab6c68405c3583ca9ae2f48eaff6f0fa72df16a
Abhay Singh <abhay2662006@gmail.com>
2025-11-14 16:14:02 +0530

    Update file.c

--- file.c
+++ file.c
@@ -14,4 +14,14 @@ int main(){
         printf("\n");
     }
 
+    printf("\n");
+
+    for(int i = 1; i<= n;i++){
+        for(int j = 1;j <= n - i ;j++){
+            printf(" ");
+        }
+        for(int j = n -i;j > 0 ;j--){
+        printf("%d" ,i);
+        }
+    }
 }
