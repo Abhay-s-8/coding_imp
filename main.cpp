a40f1f446b280d3c9bcba2deac0c8d02b8aff4e1
Abhay Singh <abhay2662006@gmail.com>
2026-01-07 07:55:22 +0530

    s
    w

--- main.cpp
+++ main.cpp
@@ -422,3 +422,40 @@ while(n>0){
 cout<<reverse<<endl;
 return 0;
 }
+
+
+// pattern printing
+//    *   
+//   ***  
+//  ***** 
+// *******
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;cin>>n;
+    
+    for(int i= n - 1;i>=0;i--){
+      
+      for(int j = 0;j<i;j++){
+        cout<<" ";
+      }
+      
+      for(int j =0 ;j< n-i;j++){
+        cout<<"*";
+      }
+      for(int j =0;j<n-i-1;j++){
+      cout<<"*";
+      }
+      for(int j=0;j<i;j++){
+        cout<<" ";
+      }
+      
+
+      
+      cout<<endl;
+      
+    }
+    return 0;
+}
