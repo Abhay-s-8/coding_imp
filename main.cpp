fb739ed4d4dc072f0754ef83bbe8661c83aab1c9
Abhay Singh <abhay2662006@gmail.com>
2026-01-07 15:41:16 +0530

    s
    s

--- main.cpp
+++ main.cpp
@@ -517,3 +517,30 @@ int main()
     }
     return 0;
 }
+
+// pattern printing
+// A
+// BB
+// CCC
+// DDDD
+// EEEEE
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;cin>>n;
+    char c = 65 ;
+    
+    for(int i= 0;i < n   ;i++){
+      char x = 0;
+      for(int j = 0 ;j <=  i;j++){
+              x = c + i ;
+      cout<< x ;
+
+      }
+
+      cout<<endl;
+    }
+    return 0;
+}
